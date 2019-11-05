a999152a02cd46fe5f5c6da03c711d4da3c1d75e
Zihao Xu <zxql2015@mymail.pomona.edu>
2019-11-05 15:29:14 -0500

    Set up directories

---
+++ uilts.py
@@ -0,0 +1,10 @@
+import sys, path, os
+import numpy as np 
+import pandas as pd 
+import matplotlib.pyplot as plt 
+import seaborn as sns
+
+# Replace these paths with your own absolute paths
+data_path = '<your_own>' #/Users/zihaoxu/data_lan/spotify_recommender/data/
+export_path = '<your_own>' #/Users/zihaoxu/data_lan/spotify_recommender/export/
+
