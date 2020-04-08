f910fc6a69f26e706f854f07c3f9d1484f4aa0c2
Zihao Xu <zxql2015@mymail.pomona.edu>
2020-04-08 11:21:20 -0400

    Setup directory

---
+++ utils.py
@@ -0,0 +1,11 @@
+
+import sys, os, gc, json, pickle
+import numpy as np 
+import pandas as pd 
+import matplotlib.pyplot as plt 
+import seaborn as sns
+
+from collections import defaultdict
+
+# Replace these paths with your own absolute paths
+data_path = '../../data/'
